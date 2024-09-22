5b7fc0fc1c497c56a4458a8480b848bae01fdfc9
gerashdo <gerardo.jimenezarguelles@gmail.com>
2024-09-21 21:16:55 -0600

    Change implementation for presentation form
    Change to use react hook form

--- src/components/presentations/NewPresentationForm.tsx
+++ src/components/presentations/NewPresentationForm.tsx
@@ -1,6 +1,8 @@
-import { useState } from "react"
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
 import { Button } from "../shared/Button"
 import { Input } from "../shared/Input"
+import { PresentationForm, validationSchema } from "../../interfaces/presentation"
 
 
 interface NewPresentationFormProps {
@@ -8,20 +10,23 @@ interface NewPresentationFormProps {
 }
 
 export const NewPresentationForm = ({onSubmit}: NewPresentationFormProps) => {
-  const [newPresentationName, setNewPresentationName] = useState('')
-  const handleSubmitNewPresentation = (e: React.FormEvent) => {
-    e.preventDefault()
-    onSubmit(newPresentationName)
+  const { register, handleSubmit, formState: { errors }, reset } = useForm<PresentationForm>({
+    resolver: yupResolver(validationSchema)
+  })
+
+  const onSubmitHandler = (data: PresentationForm) => {
+    onSubmit(data.title)
+    reset()
   }
 
   return (
-    <form onSubmit={handleSubmitNewPresentation}>
+    <form onSubmit={handleSubmit(onSubmitHandler)}>
       <div className="mb-4">
         <Input
           type="text"
-          id="presentationName"
-          value={newPresentationName}
-          onChange={(e) => setNewPresentationName(e.target.value)}
+          id="title"
+          {...register('title')}
+          error={errors.title?.message}
           placeholder="Enter name for the presentation"
           required
         />
--- src/interfaces/presentation.ts
+++ src/interfaces/presentation.ts
@@ -1,6 +1,14 @@
+import * as yup from 'yup'
+
 
 export enum AllowedPresentationOrderByFields {
   title = 'title',
   creator = 'creator',
   createdAt = 'createdAt'
 }
+
+export const validationSchema = yup.object().shape({
+  title: yup.string().required().min(5, 'Title must be at least 5 characters')
+})
+
+export type PresentationForm = yup.InferType<typeof validationSchema>
